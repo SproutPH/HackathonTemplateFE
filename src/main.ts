4d0269c5269b542530efa7cf033baa0c1537f035
Jeferson Mari <jefersonm@sprout.ph>
2025-04-22 14:22:08 +0800

    Add design-system-next integration: updated package.json and package-lock.json to include design-system-next as a dependency, modified main.ts to register the design system, and enhanced HomeView with new button components for improved UI interaction.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
+import SproutDesignSystem from 'design-system-next'; 
 
+import 'design-system-next/style.css'; 
 import "./assets/main.css";
 
 console.log(import.meta.env.VITE_API_URL);
@@ -11,4 +13,5 @@ const app = createApp(App);
 const pinia = createPinia();
 app.use(pinia);
 app.use(router);
+app.use(SproutDesignSystem);
 app.mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,19 +5,8 @@
     <p v-if="keycloakStore.isAuthenticated">
       User: {{ keycloakStore.userName }}
     </p>
-    <button type="button" @click="displayToken">Display Token</button>
-    <button type="button" @click="testFetch">Test Endpoint Call</button>
-
-    <div
-      class="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10"
-    >
-      <div>
-        <div class="text-xl font-medium text-black dark:text-white">
-          ChitChat
-        </div>
-        <p class="text-gray-500 dark:text-gray-400">You have a new message!</p>
-      </div>
-    </div>
+    <spr-button tone="success"@click="displayToken">Display Token</spr-button>
+    <spr-button tone="danger"@click="testFetch">Test Endpoint Call</spr-button>
   </div>
 </template>
 
